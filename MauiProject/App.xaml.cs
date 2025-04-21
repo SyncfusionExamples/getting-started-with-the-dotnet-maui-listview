50d1ddb434d44fad85ddc31e43d6b202f8aac524
SowntharyaJ <sowntharya.jayamoorthy@syncfusion.com>
2025-04-21 17:52:40 +0530

    Changed Sample Target Version to Latest .NET9 Target Version.

--- MauiProject/App.xaml.cs
+++ MauiProject/App.xaml.cs
@@ -4,9 +4,8 @@ public partial class App : Application
 {
 	public App()
 	{
-		Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VVhiQlFaclxJVHxOYVF2R2FJeVRzdV9HaUwgOX1dQl9hSXZTfkVrW3tacHdWRWk= ");
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new MainPage();
 	}
 }
